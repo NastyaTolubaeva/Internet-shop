6bffaf84b70c4f0ced56d5709e33b767dce85248
NastyaTolubaeva <90451284+NastyaTolubaeva@users.noreply.github.com>
2021-12-22 19:30:39 +0400

    Update 2021_12_07_073214_create_basket_product_table.php

--- database/migrations/2021_12_07_073214_create_basket_product_table.php
+++ database/migrations/2021_12_07_073214_create_basket_product_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBasketProductTable extends Migration
+class CreateBasketProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateBasketProductTable extends Migration
      */
     public function up()
     {
-        Schema::create('basket_product', function (Blueprint $table) {
+        Schema::create('basket_products', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('basket_id');
             $table->unsignedBigInteger('products_id');
